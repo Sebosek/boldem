50a59068028920d9d35abb0c034d59895d214fd9
Sebastian <ja@sebastianbusek.cz>
2023-09-20 21:25:41 +0200

    bugfix(:monocle_face:): added recipient

--- Boldem.ConsoleApp/Boldem.ConsoleApp/Models/Import/ImportRequest.cs
+++ Boldem.ConsoleApp/Boldem.ConsoleApp/Models/Import/ImportRequest.cs
@@ -2,12 +2,14 @@
 
 public class ImportRequest
 {
-    public IEnumerable<ContactRequest> Contacts { get; set; } = Enumerable.Empty<ContactRequest>();
-    
+    public IEnumerable<ContactRequest> Contacts { get; set; }
+
+    public IEnumerable<int> MailingListIds { get; set; } = new[] { 1883 };
+
     public int ContactOverwriteMode { get; set; }
     
     public int PreImportAction { get; set; }
-
+    
     public ImportRequest(IEnumerable<ContactRequest> contacts)
     {
         Contacts = contacts;
